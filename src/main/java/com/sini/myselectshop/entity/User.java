333c29707f53ce89a1ec75573fde898df70d158a
si eun <tldms123455@naver.com>
2024-02-18 20:43:24 +0900

    feat : 5-3 카카오 사용자 정보로 회원가입 구현

--- src/main/java/com/sini/myselectshop/entity/User.java
+++ src/main/java/com/sini/myselectshop/entity/User.java
@@ -1,9 +1,13 @@
 package com.sini.myselectshop.entity;
 
+import com.sini.myselectshop.entity.UserRoleEnum;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -28,10 +32,25 @@ public class User {
     @Enumerated(value = EnumType.STRING)
     private UserRoleEnum role;
 
+    private Long kakaoId;
+
     public User(String username, String password, String email, UserRoleEnum role) {
         this.username = username;
         this.password = password;
         this.email = email;
         this.role = role;
+    }
+
+    public User(String username, String password, String email, UserRoleEnum role, Long kakaoId) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.kakaoId =kakaoId;
+    }
+
+    public User kakaoIdUpdate(Long kakaoId) {
+        this.kakaoId = kakaoId;
+        return this;
     }
 }
