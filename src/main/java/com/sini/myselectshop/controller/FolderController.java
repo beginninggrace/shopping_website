0399a3dae3fc544446360b79f2dba378193d0221
si eun <tldms123455@naver.com>
2024-02-20 00:16:56 +0900

    feat : 5-10 Exception Handler 적용

--- src/main/java/com/sini/myselectshop/controller/FolderController.java
+++ src/main/java/com/sini/myselectshop/controller/FolderController.java
@@ -2,9 +2,12 @@ package com.sini.myselectshop.controller;
 
 import com.sini.myselectshop.dto.FolderRequestDto;
 import com.sini.myselectshop.dto.FolderResponseDto;
+import com.sini.myselectshop.exception.RestApiException;
 import com.sini.myselectshop.security.UserDetailsImpl;
 import com.sini.myselectshop.service.FolderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,5 +33,16 @@ public class FolderController {
     @GetMapping("/folders")
     public List<FolderResponseDto> getFolders(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         return folderService.getFolders(userDetails.getUser());
+    }
+
+    @ExceptionHandler({IllegalArgumentException.class})
+    public ResponseEntity<RestApiException> handleException(IllegalArgumentException ex) {
+        RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(
+                // HTTP body
+                restApiException,
+                // HTTP status code
+                HttpStatus.BAD_REQUEST
+        );
     }
 }
